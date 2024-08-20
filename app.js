7a1f207f8fcd16cfd72d164bdbd48375c570f961
ORazB <devandramahottama@gmail.com>
2024-08-20 18:06:12 +0800

    Clone

---
+++ app.js
@@ -0,0 +1,73 @@
+// Constants
+const slides = document.querySelectorAll(".slide__image");
+const totalSlides = slides.length;
+const leftButton = document.getElementById("left-arrow");
+const rightButton = document.getElementById("right-arrow");
+
+// Variables
+let index = 0;
+let sliderDebounce = false;
+
+// ScrollReveal
+ScrollReveal().reveal('.slider__text', {
+  origin: 'bottom',
+  distance: '50px',  
+  duration: 1250,
+  easing: 'cubic-bezier(0.5, 0, 0, 1)'
+});
+
+ScrollReveal().reveal('.arrow__wrapper', {
+  origin: 'bottom',
+  distance: '50px',  
+  duration: 1250,
+  delay: 175,
+  easing: 'cubic-bezier(0.5, 0, 0, 1)'
+});
+
+
+// Setup Slides
+slides.forEach((slide, slideIndex) => {
+  if (slideIndex === 0) {
+    slide.classList.add("active");
+  } else {
+    slide.classList.add("after-active");
+  }
+});
+function showSlide(newIndex) {
+  // Remove active class from current slide
+  slides[index].classList.remove("active");
+  slides[index].classList.add("after-active");
+
+  // Update the index
+  index = newIndex;
+
+  // Add active class to the new slide
+  slides[index].classList.remove("after-active");
+  slides[index].classList.add("active");
+}
+
+rightButton.addEventListener('click', () => {
+  if (!sliderDebounce) {
+    sliderDebounce = true;
+
+    let newIndex = (index + 1) % totalSlides;
+    showSlide(newIndex);
+
+    setTimeout(() => {
+      sliderDebounce = false;
+    }, 1000);
+  }
+});
+
+leftButton.addEventListener('click', () => {
+  if (!sliderDebounce) {
+    sliderDebounce = true;
+
+    let newIndex = (index - 1 + totalSlides) % totalSlides;
+    showSlide(newIndex);
+
+    setTimeout(() => {
+      sliderDebounce = false;
+    }, 1000);
+  }
+});
